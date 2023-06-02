5731b3540be60d1b671438447caa28ee9676a7d1
LuisDOlvera <luisd_84@hotmail.com>
2023-06-02 14:02:01 -0600

    Feat: Update Exercise 30-05-2023(2)

--- buscador.js
+++ buscador.js
@@ -1,13 +1,19 @@
 
 /*
-Ejercicio:
+ ===== Ejercicio: ====
  * Pasarle un nombre a nuestro proceso node y verificar si ese nombre existe en nuestro arreglo de nombres. 
 *Si existe imprimir 'Bienvenido nombre'. Si no existe, imprimir 'Fuera de aquí'. 
 
- * Esti tiene que estar en una función. 
-
+ * Esto tiene que estar en una función. 
 */
-const nombre = process.argv[2];
+
+/*
+* Find -> Si lo encuentra, lo trae
+* Filter -> Devuelve un arreglo con los filtrados
+* Includes -> Regresa un valor Booleano si lo encuentra devuelve True y de lo contrario     False. 
+*/
+
+const nombre = process.argv[2]; //Accediendo al 3er valor del Objeto Procesos en su propiedad argv (argumentos) con índice [2]
 
 const nombres = [
     "Andres",
